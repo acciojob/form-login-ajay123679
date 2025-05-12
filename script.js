b2517aec982faf3a06b9a0c525fadd69b69e7e4e
ajay123679 <65329197+ajay123679@users.noreply.github.com>
2025-05-12 17:35:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
+function getFormvalue(e) {
     //Write your code here
-
+    e.preventDefault();
+	const form=document.getElementById("form1");
+	const formdata=new FormData(form);
+	let arr=[];
+	for(let [key,value] of formdata)
+	{
+		arr.push(value);
+	}
+	alert(arr[0]+" "+arr[1]);
 }
